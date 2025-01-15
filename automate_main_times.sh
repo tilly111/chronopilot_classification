937148cab23245d1ab7bff6559285f684fed87ba
tilly <till.aust@uni-konstanz.de>
2025-01-15 15:33:53 +0100

    hopala

--- automate_main_times.sh
+++ automate_main_times.sh
@@ -9,7 +9,7 @@ for n_classes in 2 3; do
         for scoring in "accuracy" "roc_auc"; do
             for label in "duration_estimate" "ppot"; do
                 for experiment_time in 0 1 2; do
-                  python eye_tracking_time.py $n_classes $tw $scoring $label $robot
+                  python eye_tracking_time.py $n_classes $tw $scoring $label $experiment_time
                 done
             done
         done
